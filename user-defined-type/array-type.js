be5281dfb5d58c11d6b29d4234bbf78648973aa3
rana <rana.ahammed.012@gmail.com>
2024-01-16 17:05:33 +0600

    6th commit

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,5 @@
+// let userNames: string[];
+var userNames;
+userNames = ["anis", "rabu", "pinky"];
+console.log(userNames[0]);
+console.log(userNames.sort());
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,7 @@
+// let userNames: string[];
+let userNames: Array<string>;
+
+userNames = ["anis", "rabu", "pinky"];
+console.log(userNames[0]);
+
+console.log(userNames.sort());
