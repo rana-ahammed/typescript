1b1ab1b2651d090b98168b297463b6c924568c61
rana <rana.ahammed.012@gmail.com>
2024-01-16 12:58:38 +0600

    3rd commit

---
+++ first-program/index.js
@@ -0,0 +1,6 @@
+var addNumbers = function (num1, num2) {
+    console.log(num1 + num2);
+};
+addNumbers(25, 30);
+// addNumbers(20, "30");
+// tsc index.ts -> index.js -> node index.js
---
+++ first-program/index.ts
@@ -0,0 +1,7 @@
+const addNumbers = (num1: number, num2: number) => {
+	console.log(num1 + num2);
+};
+
+addNumbers(25, 30);
+// addNumbers(20, "30");
+// tsc index.ts -> index.js -> node index.js
